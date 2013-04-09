2b73143124e23cf9f41e645a53a5bf7205da8b09
Mariusz Nowak <mariusz@medikoo.com>
2013-04-09 12:22:26 +0200

    Tests for Password

---
+++ test/string/string-line/password.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var isError = require('es5-ext/lib/Error/is-error');
+
+module.exports = function (t, a) {
+	a(t('razdwa3'), 'razdwa3', "Valid");
+	a(isError(t.prototype.validateCreate('')), true, "Empty");
+	a(isError(t.prototype.validateCreate('razdwadasda')), true, "No digit");
+	a(isError(t.prototype.validateCreate('982389423')), true, "No a-z");
+	a(isError(t.prototype.validateCreate('raz1')), true, "Too short");
+};
