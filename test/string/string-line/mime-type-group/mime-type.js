c5a5e1c427641ea3c64c86502a63745a1cfda4a5
Mariusz Nowak <mariusz@medikoo.com>
2013-08-27 21:33:59 +0200

    Update up to changes in es5-ext package

--- test/string/string-line/mime-type-group/mime-type.js
+++ test/string/string-line/mime-type-group/mime-type.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var isError = require('es5-ext/lib/Error/is-error');
+var isError = require('es5-ext/error/is-error');
 
 module.exports = function (t, a) {
 	a(t('application/vnd.3gpp.pic-bw-large'), 'application/vnd.3gpp.pic-bw-large',
--- test/string/string-line/password.js
+++ test/string/string-line/password.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var isError = require('es5-ext/lib/Error/is-error');
+var isError = require('es5-ext/error/is-error');
 
 module.exports = function (t, a) {
 	a(t('razdwa3'), 'razdwa3', "Valid");
