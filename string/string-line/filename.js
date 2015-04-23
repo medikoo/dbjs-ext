a264056e89c331f56fcd4163941ff9b4eb69060c
Mariusz Nowak <medyk@medikoo.com>
2015-04-23 15:59:41 +0200

    Introduce adapt function for Filename

--- string/string-line/filename.js
+++ string/string-line/filename.js
@@ -6,6 +6,14 @@ var memoize          = require('memoizee/plain')
 module.exports = memoize(function (db) {
 	return defineStringLine(db).extend('Filename', {
 		pattern: { value: /^(?:[a-zA-Z]:\\)?[\u0009 -9;-\uffff]*$/ },
-		min: { value: 1 }
+		min: { value: 1 },
+		adapt: { value: function (name) {
+			var prefix = '';
+			if (name.slice(0, 2).test(/[a-zA-Z]:/)) {
+				prefix = name.slice(0, 2);
+				name = name.slice(2);
+			}
+			return prefix + String(name).replace(/[\0-\u0008\u0010-\u001f:]/g, '-');
+		}, type: db.Function }
 	});
 }, { normalizer: require('memoizee/normalizers/get-1')() });
