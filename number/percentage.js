180c0a1f9954aa763c3f7bbb2c6d706ef9c12cf7
kamsi <gruca.kamil@gmail.com>
2016-09-28 12:29:31 +0200

    #5 step can now be read from descriptor in toString

--- number/percentage.js
+++ number/percentage.js
@@ -7,7 +7,7 @@ module.exports = memoize(function (db) {
 	return validDbjs(db).Number.extend('Percentage',
 		{ min: { value: 0 }, step: { value: 0.01 } }, {
 			toString: { value: function (descriptor) {
-				var num, step = this.constructor.step, value = this * 100;
+				var num, step = descriptor.step || this.constructor.step, value = this * 100;
 				if (!step) return value + '%';
 				num = 0;
 				step *= 100;
